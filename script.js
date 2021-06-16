d604781d561bf11eb2666a11d207ff4c1b710001
Ted Heikkila <ted.heikkila@gmail.com>
2021-06-15 21:00:05 -0500

    fixed timer, ready to load 1st question

--- script.js
+++ script.js
@@ -1,46 +1,68 @@
 // js file for coding quiz, TWH, 6-15-21
 
+// instruction global var
+var instructions = document.querySelector(".instructions")
 
-// code the start button to initiate the timer
+//title global var
+var title = document.querySelector(".title")
 
+//score global var
+var score = document.querySelector("#score")
 
+// startBtn global var
+var startBtn = document.querySelector(".start-btn");
 
-
-
+// timer global var
 var timer = document.getElementById('timer');
 
-// Timer that counts down from 30 seconds; event initiated by clicking start btn
-function countdown() {
-    var timeLeft = 30;
-  
-    // uses the `setInterval()` method to call a function to be executed every 1 second
-    var timeInterval = setInterval(function () {
-      // As long as the `timeLeft` is greater than 1
-      if (timeLeft > 1) {
-        // Set the `textContent` of `timeLeft` to show the remaining seconds
-        timer.textContent = timeLeft + ' seconds remaining';
-        // Decrement `timeLeft` by 1
-        timeLeft--;
-      } else if (timeLeft === 1) {
-        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-        timer.textContent = timeLeft + ' second remaining';
-        timeLeft--;
-      } else {
-        // Once `timeLeft` gets to 0, set `timer` to an empty string
-        timer.textContent = "";
-        // Use `clearInterval()` to stop the timer
-        clearInterval(timeInterval);
-        // Call the `displayMessage()` function
-        displayQuizOver();
-      }
-      // change back to 1000 later
-    }, 100);
-  }
-
-    // Displays the quiz's final timer message; this directs user to final score screen
-    function displayQuizOver() {
-        var quizOverMessage = "Time's up!"
-        timer.textContent = quizOverMessage
-    }
+// timer that counts down from 30 seconds; event initiated by clicking start btn
+startBtn.addEventListener("click", function start() {
+    function countdown() {
+        var timeLeft = 30;
     
-  countdown();
+        // uses the `setInterval()` method to call a function to be executed every 1 second
+        var timeInterval = setInterval(function () {
+        // As long as the `timeLeft` is greater than 1
+        if (timeLeft > 1) {
+            // set the `textContent` of `timeLeft` to show the remaining seconds
+            timer.textContent = timeLeft + ' seconds remaining';
+            // Decrement `timeLeft` by 1
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            // when `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+            timer.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+        } else {
+            // once `timeLeft` gets to 0, set `timer` to an empty string
+            timer.textContent = "";
+            // use `clearInterval()` to stop the timer
+            clearInterval(timeInterval);
+            // Call the `displayMessage()` function
+            displayQuizOver();
+        }
+        // ticks every 1 second
+        }, 1000);
+    }
+        // Displays the quiz's final timer message
+        function displayQuizOver() {
+            var quizOverMessage = "Time's up!"
+            timer.textContent = quizOverMessage
+        }
+   
+        console.log(timer)
+        
+        countdown()
+ }); 
+
+// empties the start menu
+
+startBtn.addEventListener("click", function start() {
+    title.textContent = "";
+    instructions.textContent = "";
+    startBtn.remove();
+    score.textContent = "";
+});
+
+
+
+

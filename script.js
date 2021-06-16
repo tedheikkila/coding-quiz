2b8f94fad363da6f06039635acc2abd9a1a8ee09
Ted Heikkila <ted.heikkila@gmail.com>
2021-06-16 16:32:46 -0500

    created/styled question 1

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ var startBtn = document.querySelector(".start-btn");
 // timer global var
 var timer = document.getElementById('timer');
 
+// answer global var
+var answers = document.getElementById(".answers")
+
+
 // timer that counts down from XX seconds; event initiated by clicking start btn
 startBtn.addEventListener("click", function start() {
     function countdown() {
@@ -67,18 +71,64 @@ startBtn.addEventListener("click", function start() {
 
 // starts the quiz with each question as an object that is iterated until all questions are asked
 startBtn.addEventListener("click", function start() {
-
+    
     // creates element based on tag entered by user
-    var question = document.createElement("h3");
+    var question1 = document.createElement("h3");        
+    var answer1 = document.querySelector("#answer1")
+    var answer2 = document.querySelector("#answer2")
+    var answer3 = document.querySelector("#answer3")
+    var answer4 = document.querySelector("#answer4")
+
 
     // adds text content to created tag
-    question.textContent = "Commonly used data types DO NOT include:"
-  
+    question1.textContent = "Commonly used data types DO NOT include:"
+    answer1.textContent = "A. strings"
+    answer2.textContent = "B. arrays"
+    answer3.textContent = "C. prompts"
+    answer4.textContent = "D. booleans"
+
     // appends tag as child of document body
-    body.appendChild(question);
-   
+    body.appendChild(question1);
+    body.appendChild(answer1);
+    body.appendChild(answer2);
+    body.appendChild(answer3);
+    body.appendChild(answer4);
+
     // styles the question
-    question.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+    question1.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+    answer1.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; margin-top:30px; padding:10px; background-color:thistle");
+        answer1.addEventListener("mouseover", function() {
+            answer1.style.backgroundColor = "rgb(98, 248, 98)";
+        });
+
+        answer1.addEventListener("mouseout", function() {
+            answer1.style.backgroundColor = "thistle";
+            });
+    answer2.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; padding:10px; background-color:thistle");
+        answer2.addEventListener("mouseover", function() {
+        answer2.style.backgroundColor = "rgb(98, 248, 98)";
+        });
+
+         answer2.addEventListener("mouseout", function() {
+        answer2.style.backgroundColor = "thistle";
+        });
+    answer3.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; padding:10px; background-color:thistle");
+        answer3.addEventListener("mouseover", function() {
+        answer3.style.backgroundColor = "rgb(98, 248, 98)";
+    });
+
+        answer3.addEventListener("mouseout", function() {
+        answer3.style.backgroundColor = "thistle";
+    }); 
+    answer4.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; padding:10px; background-color:thistle");
+        answer4.addEventListener("mouseover", function() {
+        answer4.style.backgroundColor = "rgb(98, 248, 98)";
+        });
+
+        answer4.addEventListener("mouseout", function() {
+        answer4.style.backgroundColor = "thistle";
+        }); 
+
 
 });
 

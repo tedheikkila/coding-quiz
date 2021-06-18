a62cc5c266bb52af1a6b03d3cb18a04ffd5c3f93
Ted Heikkila <ted.heikkila@gmail.com>
2021-06-17 21:00:23 -0500

    pushing question 2 and 3 (still bugged)

--- script.js
+++ script.js
@@ -68,19 +68,34 @@ function clearStart() {
 }
 
 function clearQuestion_1() {
-    answers.remove()
-    question.remove()
 
+    question.style.display = "none"
+    answers.style.display = "none"
+    
     nextButton.setAttribute("style", "text-align: center; width:20%; margin-left:40%; margin-right:40%; margin-top:30px; padding:10px");
     nextButton.textContent = "Continue"
-
+    
     nextButton.addEventListener("click", function() {
-        question_2()
         answer_2()
+        question_2()
     });
 
 }
 
+function clearQuestion_2() {
+
+    question.style.display = "none"
+    answers.style.display = "none"
+    
+    nextButton.setAttribute("style", "text-align: center; width:20%; margin-left:40%; margin-right:40%; margin-top:30px; padding:10px");
+    nextButton.textContent = "Continue"
+    
+    nextButton.addEventListener("click", function() {
+        answer_3()
+        question_3()
+    });
+
+}
 
 // timer that counts down from 30 seconds; event initiated by clicking start btn
 function startTimer() {      
@@ -116,27 +131,62 @@ function displayQuizOver() {
 // function populates the next question (1, 2, 3, etc)
 function question_1() {
     question.textContent = quizQuestions[0]
-
     styleQuestion_1()
 }
+
+// function populates the next question (1, 2, 3, etc)
+function question_2() {
+    question.textContent = quizQuestions[1]
+    styleQuestion_2()
+}
+
+// function populates the next question (1, 2, 3, etc)
+function question_3() {
+    question.textContent = quizQuestions[2]
+    styleQuestion_3()
+}
+
 
 function answer_1 () {
     answer1.textContent = question1Answers[0]
     answer2.textContent = question1Answers[1]
     answer3.textContent = question1Answers[2]
     answer4.textContent = question1Answers[3]
-
     styleAnswer_1()
 }
 
-function styleQuestion_1() {
+function answer_2 () {
+    answer1.textContent = question2Answers[0]
+    answer2.textContent = question2Answers[1]
+    answer3.textContent = question2Answers[2]
+    answer4.textContent = question2Answers[3]
+    styleAnswer_2()
+}
 
+function answer_3 () {
+    answer1.textContent = question3Answers[0]
+    answer2.textContent = question3Answers[1]
+    answer3.textContent = question3Answers[2]
+    answer4.textContent = question3Answers[3]
+    styleAnswer_3()
+}
+
+function styleQuestion_1() {
     scoreAnswer_1()
 }
 
+function styleQuestion_2() {
+    scoreAnswer_2()
+}
+
+function styleQuestion_3() {
+    scoreAnswer_3()
+}
+
 function styleAnswer_1() {
+    answers.style.display = "block"
     // styles answer1
-    answer1.style.display = "visible"
+    answer1.style.display = "block"
     answer1.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; margin-top:30px; padding:10px; background-color:thistle");
     // styles the answers with mouseover events
     answer1.addEventListener("mouseover", function() {
@@ -148,7 +198,7 @@ function styleAnswer_1() {
     });
  
     //styles answer2
-    answer2.style.display = "visible"
+    answer2.style.display = "block"
     answer2.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; margin-top:30px; padding:10px; background-color:thistle");
     // styles the answers with mouseover events
     answer2.addEventListener("mouseover", function() {
@@ -160,7 +210,7 @@ function styleAnswer_1() {
     });
  
    //styles answer3
-   answer3.style.display = "visible"
+   answer3.style.display = "block"
    answer3.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; margin-top:30px; padding:10px; background-color:thistle");
    // styles the answers with mouseover events
    answer3.addEventListener("mouseover", function() {
@@ -172,7 +222,7 @@ function styleAnswer_1() {
    });
  
   //styles answer4
-  answer4.style.display = "visible"
+  answer4.style.display = "block"
   answer4.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; margin-top:30px; padding:10px; background-color:thistle");
   // styles the answers with mouseover events
   answer4.addEventListener("mouseover", function() {
@@ -187,15 +237,122 @@ function styleAnswer_1() {
 
 }
 
+function styleAnswer_2() {
+    answers.style.display = "block"
+    // styles answer1
+    answer1.style.display = "block"
+    answer1.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; margin-top:30px; padding:10px; background-color:thistle");
+    // styles the answers with mouseover events
+    answer1.addEventListener("mouseover", function() {
+        answer1.style.backgroundColor = "rgb(98, 248, 98)";
+    });
+    // styles the answers with mouseout events
+    answer1.addEventListener("mouseout", function() {
+        answer1.style.backgroundColor = "thistle";
+    });
+ 
+    //styles answer2
+    answer2.style.display = "block"
+    answer2.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; margin-top:30px; padding:10px; background-color:thistle");
+    // styles the answers with mouseover events
+    answer2.addEventListener("mouseover", function() {
+        answer2.style.backgroundColor = "rgb(98, 248, 98)";
+    });
+    // styles the answers with mouseout events
+    answer2.addEventListener("mouseout", function() {
+        answer2.style.backgroundColor = "thistle";
+    });
+ 
+   //styles answer3
+   answer3.style.display = "block"
+   answer3.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; margin-top:30px; padding:10px; background-color:thistle");
+   // styles the answers with mouseover events
+   answer3.addEventListener("mouseover", function() {
+        answer3.style.backgroundColor = "rgb(98, 248, 98)";
+   });
+   // styles the answers with mouseout events
+   answer3.addEventListener("mouseout", function() {
+        answer3.style.backgroundColor = "thistle";
+   });
+ 
+  //styles answer4
+  answer4.style.display = "block"
+  answer4.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; margin-top:30px; padding:10px; background-color:thistle");
+  // styles the answers with mouseover events
+  answer4.addEventListener("mouseover", function() {
+       answer4.style.backgroundColor = "rgb(98, 248, 98)";
+  });
+  // styles the answers with mouseout events
+  answer4.addEventListener("mouseout", function() {
+       answer4.style.backgroundColor = "thistle";
+  });
+ 
+  scoreAnswer_2()
+
+}
+
+function styleAnswer_3() {
+    answers.style.display = "block"
+    // styles answer1
+    answer1.style.display = "block"
+    answer1.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; margin-top:30px; padding:10px; background-color:thistle");
+    // styles the answers with mouseover events
+    answer1.addEventListener("mouseover", function() {
+        answer1.style.backgroundColor = "rgb(98, 248, 98)";
+    });
+    // styles the answers with mouseout events
+    answer1.addEventListener("mouseout", function() {
+        answer1.style.backgroundColor = "thistle";
+    });
+ 
+    //styles answer2
+    answer2.style.display = "block"
+    answer2.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; margin-top:30px; padding:10px; background-color:thistle");
+    // styles the answers with mouseover events
+    answer2.addEventListener("mouseover", function() {
+        answer2.style.backgroundColor = "rgb(98, 248, 98)";
+    });
+    // styles the answers with mouseout events
+    answer2.addEventListener("mouseout", function() {
+        answer2.style.backgroundColor = "thistle";
+    });
+ 
+   //styles answer3
+   answer3.style.display = "block"
+   answer3.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; margin-top:30px; padding:10px; background-color:thistle");
+   // styles the answers with mouseover events
+   answer3.addEventListener("mouseover", function() {
+        answer3.style.backgroundColor = "rgb(98, 248, 98)";
+   });
+   // styles the answers with mouseout events
+   answer3.addEventListener("mouseout", function() {
+        answer3.style.backgroundColor = "thistle";
+   });
+ 
+  //styles answer4
+  answer4.style.display = "block"
+  answer4.setAttribute("style", "text-align: center; width:50%; margin-left:25%; margin-right:25%; margin-top:30px; padding:10px; background-color:thistle");
+  // styles the answers with mouseover events
+  answer4.addEventListener("mouseover", function() {
+       answer4.style.backgroundColor = "rgb(98, 248, 98)";
+  });
+  // styles the answers with mouseout events
+  answer4.addEventListener("mouseout", function() {
+       answer4.style.backgroundColor = "thistle";
+  });
+ 
+  scoreAnswer_3()
+
+}
+
 function scoreAnswer_1() {
 
     answer1.addEventListener("click", function() {
         if (answer1=true) {
-        feedback.textContent = "Incorrect!"
-        score.textContent = "-5 points"
-        timerCount -= 5
-        score -= 5
-        console.log(score)
+        feedback.textContent = "Incorrect"
+        score.textContent = "-10 points"
+        timerCount -= 3
+        score -= 10
         }
         clearQuestion_1()
     });
@@ -211,25 +368,109 @@ function scoreAnswer_1() {
     });
     answer3.addEventListener("click", function() {
         if (answer3=true) {
-        feedback.textContent = "Incorrect!"
-        score.textContent = "-5 points"
-        timerCount -= 5
-        score -=5
+        feedback.textContent = "Incorrect"
+        score.textContent = "-10 points"
+        timerCount -= 3
+        score -= 10
         }
         clearQuestion_1()
     });
     answer4.addEventListener("click", function() {
         if (answer4=true) {
-        feedback.textContent = "Incorrect!"
-        score.textContent = "-5 points"
-        timerCount -=5
-        score -=5
+        feedback.textContent = "Incorrect"
+        score.textContent = "-10 points"
+        timerCount -= 3
+        score -= 10
         }
         clearQuestion_1()
     });
 }
 
+function scoreAnswer_2() {
 
+    answer1.addEventListener("click", function() {
+        if (answer1=true) {
+        feedback.textContent = "Incorrect"
+        score.textContent = "-10 points"
+        timerCount -= 3
+        score -= 10
+        console.log(score)
+        }
+        clearQuestion_2()
+    });
+
+    answer2.addEventListener("click", function() {
+        if (answer2=true) {
+        feedback.textContent = "Correct!"
+        score.textContent = "+10 points"
+        score += 10
+        //no timer deduction necessary
+        }
+        clearQuestion_2()
+    });
+    answer3.addEventListener("click", function() {
+        if (answer3=true) {
+        feedback.textContent = "Incorrect"
+        score.textContent = "-10 points"
+        timerCount -= 3
+        score -= 10
+        }
+        clearQuestion_2()
+    });
+    answer4.addEventListener("click", function() {
+        if (answer4=true) {
+        feedback.textContent = "Incorrect"
+        score.textContent = "-10 points"
+        timerCount -= 3
+        score -= 10
+        }
+        clearQuestion_2()
+    });
+}
+
+function scoreAnswer_3() {
+
+    answer1.addEventListener("click", function() {
+        if (answer1=true) {
+        feedback.textContent = "Incorrect"
+        score.textContent = "-10 points"
+        timerCount -= 3
+        score -= 10
+        }
+        form()
+    });
+
+    answer2.addEventListener("click", function() {
+        if (answer2=true) {
+        feedback.textContent = "Correct!"
+        score.textContent = "+10 points"
+        score += 10
+        }
+        form()
+    });
+    answer3.addEventListener("click", function() {
+        if (answer3=true) {
+        feedback.textContent = "Incorrect"
+        score.textContent = "-10 points"
+        timerCount -= 3
+        score -= 10
+        }
+        form()
+    });
+    answer4.addEventListener("click", function() {
+        if (answer4=true) {
+        feedback.textContent = "Incorrect"
+        score.textContent = "-10 points"
+        timerCount -= 3
+        score -= 10
+        }
+        form()
+    });
+}
+
+function form() {
+
+}
 
 
 
